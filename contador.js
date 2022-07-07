a9161e1bb07829f7772c208373639521f676a7bd
Eve <evelinheks@gmail.com>
2022-07-07 19:52:28 -0300

    Contador de caracteres

---
+++ contador.js
@@ -0,0 +1,9 @@
+
+function meuContador() {
+    var conteudo = document.getElementById('palavras').value;
+    var palavra = conteudo.split(" ");
+    var letras = conteudo.length;
+    var outras = palavra.length;
+    var resposta = document.getElementById("resposta");
+    resposta.textContent = `Caracteres: ${letras}   -   Palavras: ${outras}`
+}
